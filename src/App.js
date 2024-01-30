52696de9a14337695a40212524bbe6c22aff26f5
Gary_Yao <ygy3389@gmail.com>
2024-01-30 23:24:39 +1100

    test8

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <p>Hello World7</p>
+          <p>Hello World8</p>
         </p>
         <a
           className="App-link"
